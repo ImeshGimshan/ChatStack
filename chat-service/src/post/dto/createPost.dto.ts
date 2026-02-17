f0d5d68983943d5cc2150dc2ef750fd3d351e160
ImeshGimshan <iamcyklon@gmail.com>
2026-02-17 17:01:54 +0530

    refactor: add validation pipes to main application and enhance DTOs with validation rules

--- chat-service/src/post/dto/createPost.dto.ts
+++ chat-service/src/post/dto/createPost.dto.ts
@@ -1,5 +1,17 @@
+import { IsNotEmpty, IsOptional, IsString, Max, MaxLength } from "@nestjs/class-validator";
+
 export class CreatePostDto {
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(5000)
     content: string;
+
+    @IsString()
+    @IsOptional()
     imageId?: string;
+
+    @IsString()
+    @IsOptional()
+    @MaxLength(200)
     title?: string;
 }
--- chat-service/src/post/dto/updatePost.dto.ts
+++ chat-service/src/post/dto/updatePost.dto.ts
@@ -1,5 +1,17 @@
+import { IsOptional, IsString, Max, MaxLength } from "@nestjs/class-validator";
+
 export class UpdatePostDto {
+    @IsString()
+    @IsOptional()
+    @MaxLength(5000)
     content?: string;
+
+    @IsString()
+    @IsOptional()
     imageId?: string;
+
+    @IsString()
+    @IsOptional()
+    @MaxLength(200)
     title?: string;
 }
