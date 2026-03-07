5a069f7e01dbf4a2f65226278c5b23b7416ffc2d
ImeshGimshan <iamcyklon@gmail.com>
2026-03-07 22:59:36 +0530

    feat: update CORS configuration in main.ts files for consistent origin handling

--- media-service/src/main.ts
+++ media-service/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'process.env.CORS_ORIGIN || "*"',
+    origin: process.env.CORS_ORIGIN || '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   })
